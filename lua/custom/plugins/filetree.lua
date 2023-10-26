be55a76ad2b6c4085482ab0ad35f423d6de176d3
Ben Boelkes <bboelkes@storycountyiowa.gov>
2023-10-26 06:30:33 -0500

    Change file tree to telescope filebrowser

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,20 +1,38 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- -- Unless you are still migrating, remove the deprecated commands from v1.x
+-- vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+--
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+--
+-- return {
+--   "nvim-neo-tree/neo-tree.nvim",
+--   version = "*",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+--     "MunifTanjim/nui.nvim",
+--   },
+--   config = function()
+--     require('neo-tree').setup {}
+--
+--     vim.keymap.set("n", "<leader>t", '<Cmd>Neotree toggle<CR>')
+--   end,
+-- }
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   config = function()
-    require('neo-tree').setup {}
+    require('telescope').setup {
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+        }
+      }
+    }
 
-    vim.keymap.set("n", "<leader>t", '<Cmd>Neotree toggle<CR>')
+    require('telescope').load_extension('file_browser')
+
+    vim.keymap.set("n", "<leader>t", '<Cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>')
   end,
 }
