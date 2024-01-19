65c7e48a7f6a3414e0639087590aa32e8bf41d03
Ben Boelkes <vader699@gmail.com>
2024-01-19 17:44:00 -0600

    feat(SchemaStore): Add schemastore for json and yaml

---
+++ lua/plugins/schema-store.lua
@@ -0,0 +1,28 @@
+return {
+  'b0o/schemastore.nvim',
+  config = function()
+    require('lspconfig').jsonls.setup({
+      settings = {
+        json = {
+          schemas = require('schemastore').json_schemas(),
+          validate = true,
+        },
+      }
+    })
+
+    require('lspconfig').yamlls.setup({
+      settings = {
+        yaml = {
+          schemaStore = {
+            -- You must disable built-in schemaStore support if you want to use
+            -- this plugin and its advanced options like `ignore`.
+            enable = false,
+            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+            url = "",
+          },
+          schemas = require('schemastore').yaml.schemas(),
+        },
+      },
+    })
+  end,
+}
